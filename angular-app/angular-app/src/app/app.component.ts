f03c10f8189c22918527c267f049fdb9045cf8b8
Astrid JONATHAN <astrid.jonathan@gmail.com>
2019-10-02 15:51:22 -0400

    test du 02/10

--- angular-app/angular-app/src/app/app.component.ts
+++ angular-app/angular-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {User} from './models/User';
 
 @Component({
   selector: 'app-root',
@@ -6,6 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-app';
+
+   title = 'Gestion des utilisateurs';
+  userActif: User;
+ 
+   // -- Déclaration d'un object contact de type Contact (décrit plus haut)
+   aUser: User = {
+     id: 1,
+     name: 'Astrid JONATHAN',
+     pseudo: 'astridjonathan',
+     email: 'aj@gmail.com',
+     password: 'astrid'
+   };
+   // -- Tableau de contact
+  users: User[] = [
+     {
+       id: 1,
+       name: 'Astrid JONATHAN',
+       pseudo: 'astridjonathan',
+       email: 'aj@gmail.com',
+       password: 'astrid'
+     },
+     {
+       id: 2,
+       name: 'Hugo LIEGEARD',
+      pseudo: 'hugoliegeard',
+       email: 'hugo.j@gmail.com',
+       password: 'leFormateur'
+     },
+     {
+       id: 3,
+       name: 'Rosemael ANTHONY',
+       pseudo: 'rosemaelanthony',
+       email: 'rose.a@gmail.com',
+       password: 'rosemael'
+     },
+   ];
+   /**
+    * Cette fonction permet d'afficher le profil d'un contact
+    */
+  onSubmit(usersubmit: User) {
+     // -- Comment afficher dans la console
+    this.userActif = usersubmit;
+   }
 }
 
---
+++ angular-app/angular-app/src/app/models/User.ts
@@ -0,0 +1,8 @@
+export class User {
+        id: number;
+        name: string;
+        pseudo: string;
+        email: string;
+        password: string;
+        phone?: string;
+}
